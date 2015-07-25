2d0563aceb9b840a9b5afa82207db8b71f2669b5
Mohan <mohan.vakilsearch@gmail.com>
2015-07-25 16:05:35 +0530

    Updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.1.0'
 set :application, 'blogger'
 set :repo_url, 'git@github.com:mohanramkumar/blogger.git'
 
-set :deploy_to, '/home/deploy/blogger'
+set :deploy_to, '/var/www/apps/blogger'
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
